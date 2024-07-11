02e37a96456bce9c2378b87682c3a6d17500b0ab
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-07-11 13:44:04 -0300

    Updating .gitignore file and removing scss

--- to-do-list/src/main.js
+++ to-do-list/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import './styles/custom-bootstrap.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).use(router).mount('#app');
